d8690896f00a81cb2dcfd62d25ba4bd0663a8f6e
DevTony101 <tonycortesano@gmail.com>
2020-04-01 15:51:13 -0500

    Adding log4j logger dependency

--- src/main/java/com/goxr3plus/javafxwebbrowser/application/Main.java
+++ src/main/java/com/goxr3plus/javafxwebbrowser/application/Main.java
@@ -1,4 +1,3 @@
-
 package main.java.com.goxr3plus.javafxwebbrowser.application;
 
 import javafx.application.Application;
@@ -13,83 +12,85 @@ import main.java.com.goxr3plus.javafxwebbrowser.tools.InfoTool;
 
 /**
  * From here you start the application
- * 
+ *
  * @author Electron Tech! , GOXR3PLUS STUDIO
  *
  */
 public class Main extends Application {
-	
-	public static Stage window;
-	
-	/** The Top Bar of the Application */
-	public static TopBar topBar;
-	
-	public static BorderPane root;
-	
-	public static BorderlessScene borderlessScene;
-	
-	private final int screenMinWidth = 800 , screenMinHeight = 600;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		
-		//TopBar
-		topBar = new TopBar();
-		
-		//Root 
-		root = new BorderPane();
-		root.setStyle("-fx-background-color:#202020");
-		root.setTop(topBar);
-		root.setCenter(new WebBrowserController());
-		
-		//Prepare the Stage
-		window = primaryStage;
-		window.setTitle("JavaFX Web Browser");
-		window.setWidth(getVisualScreenWidth() * 0.9);
-		window.setHeight(getVisualScreenHeight() * 0.9);
-		window.centerOnScreen();
-		window.getIcons().add(InfoTool.getImageFromResourcesFolder("logo.png"));
-		window.centerOnScreen();
-		window.setOnCloseRequest(cl -> System.exit(0));
-		
-		
-		// Borderless Scene
-		borderlessScene = new BorderlessScene(window, StageStyle.UNDECORATED, root, screenMinWidth, screenMinHeight);
-		borderlessScene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
-		borderlessScene.setMoveControl(topBar);
-		window.setScene(borderlessScene);
-		
-		window.show();
-		
-	}
-	
-	/**
-	 * Gets the visual screen width.
-	 *
-	 * @return The screen <b>Width</b> based on the <b>visual bounds</b> of the Screen.These bounds account for objects in the native windowing system
-	 *         such as task bars and menu bars. These bounds are contained by Screen.bounds.
-	 */
-	public static double getVisualScreenWidth() {
-		return Screen.getPrimary().getVisualBounds().getWidth();
-	}
-	
-	/**
-	 * Gets the visual screen height.
-	 *
-	 * @return The screen <b>Height</b> based on the <b>visual bounds</b> of the Screen.These bounds account for objects in the native windowing
-	 *         system such as task bars and menu bars. These bounds are contained by Screen.bounds.
-	 */
-	public static double getVisualScreenHeight() {
-		return Screen.getPrimary().getVisualBounds().getHeight();
-	}
-	
-	/**
-	 * Main Method
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
-		launch(args);
-	}
+
+    public static Stage window;
+
+    /**
+     * The Top Bar of the Application
+     */
+    public static TopBar topBar;
+    public static BorderPane root;
+    public static BorderlessScene borderlessScene;
+    private final int screenMinWidth = 800, screenMinHeight = 600;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        //TopBar
+        topBar = new TopBar();
+
+        //Root 
+        root = new BorderPane();
+        root.setStyle("-fx-background-color:#202020");
+        root.setTop(topBar);
+        root.setCenter(new WebBrowserController());
+
+        //Prepare the Stage
+        window = primaryStage;
+        window.setTitle("JavaFX Web Browser");
+        window.setWidth(getVisualScreenWidth() * 0.9);
+        window.setHeight(getVisualScreenHeight() * 0.9);
+        window.centerOnScreen();
+        window.getIcons().add(InfoTool.getImageFromResourcesFolder("logo.png"));
+        window.centerOnScreen();
+        window.setOnCloseRequest(cl -> System.exit(0));
+
+        // Borderless Scene
+        borderlessScene = new BorderlessScene(window, StageStyle.UNDECORATED, root, screenMinWidth, screenMinHeight);
+        borderlessScene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
+        borderlessScene.setMoveControl(topBar);
+        window.setScene(borderlessScene);
+
+        window.show();
+
+    }
+
+    /**
+     * Gets the visual screen width.
+     *
+     * @return The screen <b>Width</b> based on the <b>visual bounds</b> of the
+     * Screen.These bounds account for objects in the native windowing system
+     * such as task bars and menu bars. These bounds are contained by
+     * Screen.bounds.
+     */
+    public static double getVisualScreenWidth() {
+        return Screen.getPrimary().getVisualBounds().getWidth();
+    }
+
+    /**
+     * Gets the visual screen height.
+     *
+     * @return The screen <b>Height</b> based on the <b>visual bounds</b> of the
+     * Screen.These bounds account for objects in the native windowing system
+     * such as task bars and menu bars. These bounds are contained by
+     * Screen.bounds.
+     */
+    public static double getVisualScreenHeight() {
+        return Screen.getPrimary().getVisualBounds().getHeight();
+    }
+
+    /**
+     * Main Method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+        launch(args);
+    }
 }
--- src/main/java/com/goxr3plus/javafxwebbrowser/application/controllers/TopBar.java
+++ src/main/java/com/goxr3plus/javafxwebbrowser/application/controllers/TopBar.java
@@ -4,8 +4,7 @@
 package main.java.com.goxr3plus.javafxwebbrowser.application.controllers;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.log4j.Logger;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,47 +18,41 @@ import main.java.com.goxr3plus.javafxwebbrowser.tools.InfoTool;
  * @author GOXR3PLUS
  */
 public class TopBar extends BorderPane {
-	
-	// ----------------------------------------------
-	
-	@FXML
-	private Label label;
-	
-	// ----------------------------------------------
-	
-	/** The logger. */
-	private Logger logger = Logger.getLogger(getClass().getName());
-	
-	/**
-	 * Constructor.
-	 */
-	public TopBar() {
-		
-		//---------------------FXML LOADER---------------------------------
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(InfoTool.FXMLS + "TopBar.fxml"));
-		loader.setController(this);
-		loader.setRoot(this);
-		
-		try {
-			loader.load();
-		} catch (IOException ex) {
-			logger.log(Level.WARNING, "", ex);
-		}
-	}
-	
-	/**
-	 * Called as soon as .fxml is initialized [[SuppressWarningsSpartan]]
-	 */
-	@FXML
-	private void initialize() {
-		
-		//Root
-		setRight(new CloseAppBox());
-		
-	}
-	
-	public void setTitle(String title) {
-		label.setText(title);
-	}
-	
+
+    // ----------------------------------------------
+    @FXML
+    private Label label;
+
+    // ----------------------------------------------
+    /**
+     * The logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(TopBar.class);
+
+    /**
+     * Constructor.
+     */
+    public TopBar() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(InfoTool.FXMLS + "TopBar.fxml"));
+        loader.setController(this);
+        loader.setRoot(this);
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            LOGGER.error(ex.getMessage());
+        }
+    }
+
+    /**
+     * Called as soon as .fxml is initialized [[SuppressWarningsSpartan]]
+     */
+    @FXML
+    private void initialize() {
+        //Root
+        setRight(new CloseAppBox());
+    }
+
+    public void setTitle(String title) {
+        label.setText(title);
+    }
 }
