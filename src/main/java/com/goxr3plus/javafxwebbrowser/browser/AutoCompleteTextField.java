a6067a86d721184ac47daa3fe2dc4cf0fbe182de
DevTony101 <tonycortesano@gmail.com>
2020-04-01 16:54:35 -0500

    Minor refactoring. Creating new function to validate keyCode on autocomplete

--- src/main/java/com/goxr3plus/javafxwebbrowser/browser/AutoCompleteTextField.java
+++ src/main/java/com/goxr3plus/javafxwebbrowser/browser/AutoCompleteTextField.java
@@ -1,6 +1,3 @@
-/*
- * 
- */
 package main.java.com.goxr3plus.javafxwebbrowser.browser;
 
 import java.util.List;
@@ -17,229 +14,227 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import org.apache.log4j.Logger;
 
 /**
  * The Class AutoCompleteTextField.
  *
  * @author GOXR3PLUS
  */
-/**
- * @author GOXR3PLUS
- *
- */
 public final class AutoCompleteTextField {
-	
-	/** The existing auto complete entries. in text field */
-	private SortedSet<String> entries = new TreeSet<>();
-	
-	/** The pop up used to select an entry. */
-	private final ContextMenu contextMenu = new ContextMenu();
-	
-	/** The maximum entries. */
-	private int maximumEntries = 15;
-	
-	private final StringBuilder stringBuilder = new StringBuilder();
-	
-	/** The last length. */
-	private int lastLength;
-	
-	private TextField textField;
-	
-	//-----------------------------------------------------------------------------------
-	
-	/**
-	 * Text Listener
-	 */
-	private final InvalidationListener textListener = v -> {
-		if (textField.getText().length() == 0 || entries.isEmpty())
-			contextMenu.hide();
-		else {
-			populatePopup();
-			if (!contextMenu.isShowing()) {
-				contextMenu.show(textField, Side.BOTTOM, 0, 0);
-				// Request focus on first item
-				if (!contextMenu.getItems().isEmpty())
-					contextMenu.getSkin().getNode().lookup(".menu-item:nth-child(1)").requestFocus();
-			}
-		}
-		
-	};
-	
-	/**
-	 * Focus Listener
-	 */
-	private final InvalidationListener focusListener = v -> {
-		lastLength = 0;
-		stringBuilder.delete(0, stringBuilder.length());
-		contextMenu.hide();
-	};
-	
-	/**
-	 * !!!!!!!!!!!!!!!!BUGGED !!!!!!!!!!!!!!!!! NEEDS FIXING KeyHandler
-	 */
-	private EventHandler<? super KeyEvent> keyHandler = key -> {
-		KeyCode k = key.getCode();
-		
-		// This variable is used to bypass the auto complete process if the
-		// length is the same. this occurs if user types fast, the length of
-		// textfield will record after the user has typed after a certain
-		// delay.
-		if (lastLength != ( textField.getLength() - textField.getSelectedText().length() ))
-			lastLength = textField.getLength() - textField.getSelectedText().length();
-		
-		boolean pass = true;
-		System.out.println(k.getName());
-		
-		// Not causing problems by these buttons
-		if (key.isControlDown() || k == KeyCode.BACK_SPACE || k == KeyCode.RIGHT || k == KeyCode.LEFT || k == KeyCode.DELETE || k == KeyCode.HOME || k == KeyCode.END
-				|| k == KeyCode.TAB) {
-			pass = false;
-		}
-		
-		if (pass) {
-			IndexRange indexRange = textField.getSelection();
-			stringBuilder.delete(0, stringBuilder.length());
-			stringBuilder.append(textField.getText());
-			// remove selected string index until end so only unselected text
-			// will be recorded
-			try {
-				stringBuilder.delete(indexRange.getStart(), stringBuilder.length());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-			String originalLowered = textField.getText().toLowerCase();
-			// Select the first Matching
-			for (String entry : entries)
-				if (entry.toLowerCase().startsWith(originalLowered)) {
-					try {
-						textField.setText(entry);
-					} catch (Exception e) {
-						textField.setText(stringBuilder.toString());
-					}
-					textField.positionCaret(stringBuilder.toString().length());
-					textField.selectEnd();
-					break;
-				}
-			
-			System.out.println("Passed...");
-		}
-	};
-	
-	//--------------------------------------------------------------------------------------
-	
-	/**
-	 * Add auto completion capabilities to the given textField
-	 * 
-	 * @param textField
-	 * @param maximumEntries
-	 * @param addKeyListener
-	 *            !NOT IMPLEMENTED!
-	 * @param list
-	 *            W
-	 */
-	public void bindAutoCompletion(TextField textField , int maximumEntries , boolean addKeyListener , List<String> list) {
-		entries.addAll(list);
-		bindAutoCompletion(textField, maximumEntries, addKeyListener);
-	}
-	
-	/**
-	 * <b> Warning with this method will lead to NullPointerException if the given SortedSet goes to null , i am using it when i have really big
-	 * TreeSets on my Application</b> <br>
-	 * 
-	 * Add auto completion capabilities to the given textField
-	 * 
-	 * @param textField
-	 * @param maximumEntries
-	 * @param addKeyListener
-	 *            !NOT IMPLEMENTED!
-	 * @param list
-	 */
-	public void bindAutoCompletion(TextField textField , int maximumEntries , boolean addKeyListener , SortedSet<String> sortedSet) {
-		entries = sortedSet;
-		bindAutoCompletion(textField, maximumEntries, addKeyListener);
-	}
-	
-	/**
-	 * Add auto completion capabilities to the given textField
-	 * 
-	 * @param textField
-	 * @param maximumEntries
-	 * @param addKeyListener
-	 *            !NOT IMPLEMENTED!
-	 */
-	public void bindAutoCompletion(TextField textField , int maximumEntries , boolean addKeyListener) {
-		this.textField = textField;
-		this.maximumEntries = maximumEntries <= 0 ? 10 : maximumEntries;
-		
-		// TextChanged Listener
-		textField.textProperty().addListener(textListener);
-		
-		// FocusListener
-		textField.focusedProperty().addListener(focusListener);
-		
-		//Add keyHandler?
-		//if (addKeyListener)
-		//	textField.addEventHandler(KeyEvent.KEY_RELEASED, keyHandler);
-		
-		//if(addAutoLearner) 
-		//	textField.addEventHandler(KeyEvent.KEY_RELEASED, keyHandler);
-		
-	}
-	
-	/**
-	 * Remove autoCompletion listeners from the TextField
-	 */
-	public void removeAutoCompletion() {
-		
-		// TextChanged Listener
-		textField.textProperty().removeListener(textListener);
-		
-		// FocusListener
-		textField.focusedProperty().removeListener(focusListener);
-		
-		//Add keyHandler?
-		textField.removeEventHandler(KeyEvent.KEY_RELEASED, keyHandler);
-	}
-	
-	/**
-	 * Get the existing set of auto complete entries.
-	 * 
-	 * @return The existing auto complete entries.
-	 */
-	public SortedSet<String> getEntries() {
-		return entries;
-	}
-	
-	/**
-	 * @return the maximumEntries
-	 */
-	public int getMaximumEntries() {
-		return maximumEntries;
-	}
-	
-	/**
-	 * @param maximumEntries
-	 *            the maximumEntries to set
-	 */
-	public void setMaximumEntries(int maximumEntries) {
-		this.maximumEntries = maximumEntries;
-	}
-	
-	/**
-	 * Populate the entry set with the given search results.
-	 */
-	private void populatePopup() {
-		contextMenu.getItems().clear();
-		
-		String text = textField.getText().toLowerCase();
-		
-		// Filter the first maximumEntries matching the text
-		contextMenu.getItems()
-				.addAll(entries.stream().filter(string -> string.toLowerCase().contains(text.toLowerCase())).limit(maximumEntries).map(MenuItem::new).collect(Collectors.toList()));
-		contextMenu.getItems().forEach(item -> item.setOnAction(a -> {
-			textField.setText(item.getText());
-			textField.positionCaret(textField.getLength());
-		}));
-	}
+
+    /**
+     * The existing auto complete entries. in text field
+     */
+    private SortedSet<String> entries = new TreeSet<>();
+
+    /**
+     * The pop up used to select an entry.
+     */
+    private final ContextMenu contextMenu = new ContextMenu();
+
+    /**
+     * The maximum entries.
+     */
+    private int maximumEntries = 15;
+    private final StringBuilder stringBuilder = new StringBuilder();
+
+    /**
+     * The last length.
+     */
+    private int lastLength;
+    private TextField textField;
+
+    public static final Logger LOGGER = Logger.getLogger(AutoCompleteTextField.class.getName());
+
+    //-----------------------------------------------------------------------------------
+    /**
+     * Text Listener
+     */
+    private final InvalidationListener textListener = v -> {
+        if (textField.getText().length() == 0 || entries.isEmpty()) {
+            contextMenu.hide();
+        } else {
+            populatePopup();
+            if (!contextMenu.isShowing()) {
+                contextMenu.show(textField, Side.BOTTOM, 0, 0);
+                // Request focus on first item
+                if (!contextMenu.getItems().isEmpty()) {
+                    contextMenu.getSkin().getNode().lookup(".menu-item:nth-child(1)").requestFocus();
+                }
+            }
+        }
+    };
+
+    /**
+     * Focus Listener
+     */
+    private final InvalidationListener focusListener = v -> {
+        lastLength = 0;
+        stringBuilder.delete(0, stringBuilder.length());
+        contextMenu.hide();
+    };
+
+    /**
+     * !!!!!!!!!!!!!!!!BUGGED !!!!!!!!!!!!!!!!! NEEDS FIXING KeyHandler
+     */
+    private final EventHandler<? super KeyEvent> keyHandler = key -> {
+        KeyCode keyCode = key.getCode();
+
+        // This variable is used to bypass the auto complete process if the
+        // length is the same. this occurs if user types fast, the length of
+        // textfield will record after the user has typed after a certain
+        // delay.
+        if (lastLength != (textField.getLength() - textField.getSelectedText().length())) {
+            lastLength = textField.getLength() - textField.getSelectedText().length();
+        }
+        
+        LOGGER.info(String.format("KeyCode: %s", keyCode.getName()));
+        if (!(key.isControlDown() || isKeyNotAllowed(keyCode))) {
+            LOGGER.info("KeyCode passed succesfully!");
+            IndexRange indexRange = textField.getSelection();
+            stringBuilder.delete(0, stringBuilder.length());
+            stringBuilder.append(textField.getText());
+            // remove selected string index until end so only unselected text
+            // will be recorded
+            try {
+                stringBuilder.delete(indexRange.getStart(), stringBuilder.length());
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage());
+            }
+
+            String originalLowered = textField.getText().toLowerCase();
+            // Select the first Matching
+            for (String entry : entries) {
+                if (entry.toLowerCase().startsWith(originalLowered)) {
+                    try {
+                        textField.setText(entry);
+                    } catch (Exception e) {
+                        textField.setText(stringBuilder.toString());
+                    }
+                    textField.positionCaret(stringBuilder.toString().length());
+                    textField.selectEnd();
+                    break;
+                }
+            }
+        }
+    };
+
+    public boolean isKeyNotAllowed(KeyCode keyCode) { // Not causing problems by these buttons
+        return (keyCode == KeyCode.BACK_SPACE
+                || keyCode == KeyCode.RIGHT
+                || keyCode == KeyCode.LEFT
+                || keyCode == KeyCode.DELETE
+                || keyCode == KeyCode.HOME
+                || keyCode == KeyCode.END
+                || keyCode == KeyCode.TAB);
+    }
+
+    //--------------------------------------------------------------------------------------
+    /**
+     * Add auto completion capabilities to the given textField
+     *
+     * @param textField
+     * @param maximumEntries
+     * @param addKeyListener !NOT IMPLEMENTED!
+     * @param list W
+     */
+    public void bindAutoCompletion(TextField textField, int maximumEntries, boolean addKeyListener, List<String> list) {
+        entries.addAll(list);
+        bindAutoCompletion(textField, maximumEntries, addKeyListener);
+    }
+
+    /**
+     * <b> Warning with this method will lead to NullPointerException if the
+     * given SortedSet goes to null , i am using it when i have really big
+     * TreeSets on my Application</b> <br>
+     *
+     * Add auto completion capabilities to the given textField
+     *
+     * @param textField
+     * @param maximumEntries
+     * @param addKeyListener !NOT IMPLEMENTED!
+     * @param sortedSet
+     */
+    public void bindAutoCompletion(TextField textField, int maximumEntries, boolean addKeyListener, SortedSet<String> sortedSet) {
+        entries = sortedSet;
+        bindAutoCompletion(textField, maximumEntries, addKeyListener);
+    }
+
+    /**
+     * Add auto completion capabilities to the given textField
+     *
+     * @param textField
+     * @param maximumEntries
+     * @param addKeyListener !NOT IMPLEMENTED!
+     */
+    public void bindAutoCompletion(TextField textField, int maximumEntries, boolean addKeyListener) {
+        this.textField = textField;
+        this.maximumEntries = maximumEntries <= 0 ? 10 : maximumEntries;
+
+        // TextChanged Listener
+        textField.textProperty().addListener(textListener);
+
+        // FocusListener
+        textField.focusedProperty().addListener(focusListener);
+
+        //Add keyHandler?
+        //if (addKeyListener)
+        //	textField.addEventHandler(KeyEvent.KEY_RELEASED, keyHandler);
+        //if(addAutoLearner) 
+        //	textField.addEventHandler(KeyEvent.KEY_RELEASED, keyHandler);
+    }
+
+    /**
+     * Remove autoCompletion listeners from the TextField
+     */
+    public void removeAutoCompletion() {
+        // TextChanged Listener
+        textField.textProperty().removeListener(textListener);
+
+        // FocusListener
+        textField.focusedProperty().removeListener(focusListener);
+
+        //Add keyHandler?
+        textField.removeEventHandler(KeyEvent.KEY_RELEASED, keyHandler);
+    }
+
+    /**
+     * Get the existing set of auto complete entries.
+     *
+     * @return The existing auto complete entries.
+     */
+    public SortedSet<String> getEntries() {
+        return entries;
+    }
+
+    /**
+     * @return the maximumEntries
+     */
+    public int getMaximumEntries() {
+        return maximumEntries;
+    }
+
+    /**
+     * @param maximumEntries the maximumEntries to set
+     */
+    public void setMaximumEntries(int maximumEntries) {
+        this.maximumEntries = maximumEntries;
+    }
+
+    /**
+     * Populate the entry set with the given search results.
+     */
+    private void populatePopup() {
+        contextMenu.getItems().clear();
+        String text = textField.getText().toLowerCase();
+
+        // Filter the first maximumEntries matching the text
+        contextMenu.getItems()
+                .addAll(entries.stream().filter(string -> string.toLowerCase().contains(text.toLowerCase())).limit(maximumEntries).map(MenuItem::new).collect(Collectors.toList()));
+        contextMenu.getItems().forEach(item -> item.setOnAction(a -> {
+            textField.setText(item.getText());
+            textField.positionCaret(textField.getLength());
+        }));
+    }
 }
--- src/main/java/com/goxr3plus/javafxwebbrowser/tools/Constants.java
+++ src/main/java/com/goxr3plus/javafxwebbrowser/tools/Constants.java
@@ -5,11 +5,11 @@ package main.java.com.goxr3plus.javafxwebbrowser.tools;
  * @author Tony Manjarres
  */
 public final class Constants {
-    
+
     public static final String TOPBAR_FXML = InfoTool.FXMLS + "TopBar.fxml";
     public static final String CLOSEAPP_FXML = InfoTool.FXMLS + "CloseAppBox.fxml";
-    
-    private Constants(){
-        
+    public static final String WEBBROWSER_FXML = InfoTool.FXMLS + "WebBrowserController.fxml";
+
+    private Constants() {
     }
 }
